d35b16d84922b8d0c49653d61087f3b235345241
OmarMesqq <omarmsqt@gmail.com>
2023-08-04 20:53:20 -0300

    feat: :sparkles: refatora funcao p/ obter piada
    coloca dois arrays: um para intro e outro pra punchline

--- punchlines.py
+++ punchlines.py
@@ -1,4 +1,28 @@
 import random
+FORMAT = 'utf-8'
+
+def getJoke():
+    """
+    Retorna uma tupla em bytes com a chave e o valor da piada
+    """
+    arraySize = range(len(animals))
+    randomIndex = random.choice(arraySize)
+    key =  animals[randomIndex]
+    return (key.encode(FORMAT), jokes[key].encode(FORMAT))
+
+
+animals = [
+    "A mosca!",
+    "O cavalo!",
+    "A uva!",
+    "O macaco!",
+    "O pato!",
+    "A aranha!",
+    "O milho!",
+    "O elefante!",
+    "A abelha!",
+    "O canguru!"
+]
 
 punchlines = [
     "O azar da mosca!",
@@ -13,10 +37,14 @@ punchlines = [
     "O canguru que veio entregar uma carta!"
 ]
 
-random_punchline = f"{random.choice(punchlines)}" + "\n\n"
 
-def getPunchline() -> bytes:
-    """
-    Retorna uma punchline aleatória em bytes (UTF-8)
-    """
-    return random_punchline.encode('utf-8')
+# Dicionário com compreensão para as piadas
+# Um jeito pythonico de declarar um dicionário
+jokes = {animal: punchline for animal, punchline in zip(animals, punchlines)}
+
+
+# Teste do strict no zip
+
+# Piadas internas da empresa
+# calindra = ["Cachorro", "Mané"]
+# calindra_punchlines = ["Que comeu sua branch", "que programa"]
