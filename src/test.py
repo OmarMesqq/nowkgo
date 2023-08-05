6ef4b01abc98e63b2cdab913189fdae5d918edca
OmarMesqq <omarmsqt@gmail.com>
2023-08-05 18:32:32 -0300

    refactor: :truck: muda arquivos de codigo pra src

---
+++ src/test.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import patch
+from server import bindPort, HOST
+import socket
+
+class TestServer(unittest.TestCase):
+    @patch('socket.socket.bind')
+    def test_bindPort(self, mock_bind):
+        mock_bind.side_effect = [OSError, None]
+        server = socket.socket()
+        port = 8000
+        bindPort(server, port)
+        mock_bind.assert_called_with((HOST, port + 1))
+
+if __name__ == "__main__":
+    unittest.main()
--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from .. import server
-
-# class TestMain(unittest.TestCase):
-#     def test_add_numbers(self):
-        
-
-if __name__ == "__main__":
-    unittest.main()
