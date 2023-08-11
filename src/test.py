32faa0a58656b5f0cfbda9a3980b746f0fd8059a
OmarMesqq <omarmsqt@gmail.com>
2023-08-11 18:59:23 -0300

    refactor: :recycle: cria socket de um jeito pythonico
    alem disso, adiciona docstring à classe de teste

--- src/test.py
+++ src/test.py
@@ -4,16 +4,20 @@ from server import bindPort, HOST
 import socket
 
 class TestServer(unittest.TestCase):
+    """
+    Testa a resiliência do servidor caso o SO não deixe bindar a porta de operação
+    """
     @patch('socket.socket.bind')
     def test_bindPort(self, mock_bind):
-        mock_bind.side_effect = [OSError, None]
-        server = socket.socket()
-        port = 8000
-        bindPort(server, port)
-        mock_bind.assert_called_with((HOST, port + 1))
+        mock_bind.side_effect = [OSError]
+        with socket.socket() as server:
+            port = 8000
+            bindPort(server, port)
+            mock_bind.assert_called_with((HOST, port + 1))
+
 
 if __name__ == "__main__":
     unittest.main()
 
 # Teste do strict no zip do punchlines
-# Implementar cobertura
+# Implementar cobertura 
