b7ded0f99ad8969c562b74c00346724a6dd49747
OmarMesqq <omarmsqt@gmail.com>
2023-08-12 18:59:22 -0300

    test: :sparkles: introduz teste de dicionario

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 from server import bindPort, HOST
 import socket
+from jokes import setups, punchlines
 
 class TestServer(unittest.TestCase):
     """
@@ -9,15 +10,26 @@ class TestServer(unittest.TestCase):
     """
     @patch('socket.socket.bind')
     def test_bindPort(self, mock_bind):
-        mock_bind.side_effect = [OSError]
+        mock_bind.side_effect = [OSError, None]
         with socket.socket() as server:
-            port = 8000
+            port = 9001
             bindPort(server, port)
             mock_bind.assert_called_with((HOST, port + 1))
+
+
+class TestJokeDb(unittest.TestCase):
+    def test_strictZipDict(self):
+        """
+        Avalia se a taxa setup/punchline está correta (1:1) 
+        """
+        try:
+            mockDict = {setup: punchline for setup, punchline in zip(setups, punchlines, strict=True)}
+        except ValueError:
+            print("O número de setups não bate com o de punchlines!")
 
 
 if __name__ == "__main__":
     unittest.main()
 
-# Teste do strict no zip do punchlines
+
 # Implementar cobertura 
