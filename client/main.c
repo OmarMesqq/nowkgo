e8b1795e7ac1bbcf0002cb449fe41e3faf7ce301
OmarMesqq <omarmsqt@gmail.com>
2023-09-07 19:03:40 -0300

    feat: :rocket: finaliza cliente em C de forma mais desacoplada

--- client/main.c
+++ client/main.c
@@ -1,13 +1,12 @@
-#include <stdio.h>      // Include standard I/O for printf and other functions
-#include <stdlib.h>     // Include standard library for exit and other utility functions
-#include <string.h>     // Include string.h for string manipulation functions like strlen
-#include <unistd.h>     // Include unistd.h for close function
-#include <arpa/inet.h>  // Include arpa/inet.h for inet_pton and other network functions
-// Include sys/types and sys/socket for socket programming types and functions
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <string.h>     
+#include <unistd.h>     
+#include <arpa/inet.h>  
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define MAX_INPUT_SIZE 100 
+#define MAX_INPUT_SIZE 1024 
 
 static const char HOST[10] = "127.0.0.1";
 
@@ -29,52 +28,70 @@ int setupClient() {
     return client_socket;
 }
 
+void displayServerBuffer(int client_socket, char* server_buffer, size_t buffer_size) {
+    ssize_t bytes_received; // independente de plataforma: define tamanho maximo que permite I/O
+
+    // if (bytes_received <= 0) {
+    //     perror("Erro na conexão: o servidor não mandou dados ou a conexão foi interrompida");
+    //     return;
+    // }
+
+    bytes_received = recv(client_socket, server_buffer, buffer_size - 1, 0);
+    server_buffer[bytes_received] = '\0'; 
+    printf("%s\n", server_buffer);
+}
+
+void getClientBuffer(char* client_buffer, size_t buffer_size, int client_socket) {
+    if (fgets(client_buffer, buffer_size, stdin) == NULL) {
+            return;
+        }
+        client_buffer[strcspn(client_buffer, "\n")] = '\0';
+        if (strcmp(client_buffer, "exit") == 0) {
+            return;
+        }
+        if (send(client_socket, client_buffer, strlen(client_buffer), 0) < 0) {
+            perror("Não foi possível mandar mensagem para o servidor!");
+            exit(EXIT_FAILURE);
+        }
+}
+
 int main() {
     char client_buffer[MAX_INPUT_SIZE]; // buffer p/ guardar o que o cliente digita 
-    char server_buffer[1024] = {0}; // inicializa buffer p/ guardar msgs recebidas para todo de zeros 
-    ssize_t bytes_received; // independente de plataforma: define tamanho maximo que permite I/O
+    size_t client_buffer_size = sizeof(client_buffer);
     int client_socket = setupClient();
+
+    char server_buffer[1024] = {0}; // inicializa buffer p/ guardar msgs recebidas para todo de zeros 
+    size_t server_buffer_size = sizeof(server_buffer);
     struct sockaddr_in server_address = setupServer();
 
     printf("Seja bem vindo(a) ao servidor de piadas!\nVocê tem 1 minuto entre conversas para não explodir :)\n");
     
-    // Converte HOST para binário e guarda no struct
     if (inet_pton(AF_INET, HOST, &server_address.sin_addr) <= 0) {
         perror("Endereço de IP inválido! Verifique se o entrou corretamente.");
         exit(EXIT_FAILURE);
     }
 
-    // Abre uma conexão no socket do cliente para o do servidor.
-    // Passa ponteiro do servidor para função connect. 
-    // Ele sofre um casting (esse é  seguro) para um struct mais genérico para sockets.  
-    // Como passamos um endereço na memória, temos que especificar tamanho com sizeof() da 
-    // estrutura para connect()
     if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
         perror("Não foi possível conectar ao servidor!");
         exit(EXIT_FAILURE);
     }
 
     while (1) {
+        displayServerBuffer(client_socket, server_buffer, server_buffer_size);
+
         printf("> ");
+        getClientBuffer(client_buffer, client_buffer_size, client_socket);
 
-        if (fgets(client_buffer, sizeof(client_buffer), stdin) == NULL) {
-            break;
-        }
+        displayServerBuffer(client_socket, server_buffer, server_buffer_size);
 
-        client_buffer[strcspn(client_buffer, "\n")] = '\0';
+        printf("> ");
+        getClientBuffer(client_buffer, client_buffer_size, client_socket);
 
-        if (strcmp(client_buffer, "exit") == 0) {
-            break;
-        }
+        displayServerBuffer(client_socket, server_buffer, server_buffer_size);
 
-        // Manda todos os bytes (strlen(client_buffer)) de 
-        // client_buffer para socket do cliente e, consequentemente, para servidor
-        if (send(client_socket, client_buffer, strlen(client_buffer), 0) < 0) {
-            perror("Não foi possível mandar mensagem para o servidor!");
-            exit(EXIT_FAILURE);
-        }
+        break;
     }
-    
+
     close(client_socket);
     printf("\nAgradecemos a preferência! Volte logo!\n");
     return 0;
