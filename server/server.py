cc5085314001722662a556d3cb4a6a93d6afcf3b
OmarMesqq <omarmsqt@gmail.com>
2023-09-16 17:48:40 -0300

    feat: :sparkles: Resolve debito tecnico do #1
    Implementa semaforo, max de conexoes simultaneas e usa backlog do sistema como fila de espera

--- server/server.py
+++ server/server.py
@@ -4,6 +4,9 @@ from jokes import getJoke
 
 HOST = "127.0.0.1"
 FORMAT = 'utf-8'
+MAX_CONNECTIONS = 5     # Número máximo de conexões simultâneas
+server = None 
+semaphore = threading.Semaphore(MAX_CONNECTIONS)
 
 def bindPort(server, port):
     """    
@@ -24,11 +27,12 @@ def main():
     Inicializa o servidor
     """
     port = 9001
-    
+
+    global server
     server = socket.socket()    # Cria socket IPv4 TCP
     bindPort(server, port)
     
-    server.listen(5)  
+    server.listen(10)       # Número máximo de clientes na fila de espera  
     count = 0
     print("[*] Esperando conexões...")
 
@@ -44,44 +48,41 @@ def handle_client(conn, addr, count):
     """
     Lida com a conexão de um cliente
     """
-    print(f"[*] Cliente novo (número {count}) na porta {addr[1]}")
-    intro, punchline = getJoke()
+    with semaphore:
+        print(f"[*] Cliente novo (número {count}) na porta {addr[1]}")
+        intro, punchline = getJoke()
 
-    try: 
-        conn.sendall(b"> " + b"Toc Toc") 
-
-        msg = conn.recv(1024)     # Bloqueante
-        msg = msg.decode(FORMAT)
-        print(f"[*] Cliente {count} diz: {msg}") 
-
-        conn.sendall(b"> " + intro)
-
-        msg = conn.recv(1024)     # Bloqueante
-        msg = msg.decode(FORMAT) 
-        print(f"[*] Cliente {count} diz: {msg}")
-
-        conn.sendall(b"> " + punchline)
- 
-        print(f"[*] Conexão com cliente {count} terminou com sucesso")
-    except socket.timeout:
-        conn.sendall(b"> " + b"VOCE EXPLODIU!")
-        print(f"[*] Cliente {count} desconectado por inatividade")
-    except (BrokenPipeError, UnicodeDecodeError):
-        print(f"[*] Cliente {count} encerrou a conexão") 
-    except Exception as e:
-        print(f"[!] Erro desconhecido entre cliente e servidor:\n", e)
-    finally:
-        conn.close()
+        try: 
+            conn.sendall(b"> " + b"Toc Toc") 
+    
+            msg = conn.recv(1024)     # Bloqueante
+            msg = msg.decode(FORMAT)
+            print(f"[*] Cliente {count} diz: {msg}") 
+    
+            conn.sendall(b"> " + intro)
+    
+            msg = conn.recv(1024)     # Bloqueante
+            msg = msg.decode(FORMAT) 
+            print(f"[*] Cliente {count} diz: {msg}")
+    
+            conn.sendall(b"> " + punchline)
+    
+            print(f"[*] Conexão com cliente {count} terminou com sucesso")
+        except socket.timeout:
+            conn.sendall(b"> " + b"VOCE EXPLODIU!")
+            print(f"[*] Cliente {count} desconectado por inatividade")
+        except (BrokenPipeError, UnicodeDecodeError):
+            print(f"[*] Cliente {count} encerrou a conexão") 
+        except Exception as e:
+            print(f"[!] Erro desconhecido entre cliente e servidor:\n", e)
+        finally:
+            conn.close()
     
 
 if __name__ == '__main__':
     try:
         main() 
     except KeyboardInterrupt: 
+        if server: server.close()
         print("\n[*] Saindo...")
         exit(0)
-
-
-## TO-DO:
-    # Sistema de filas do teatro
-    # Avaliar backlog/conexões simultâneas e rate limit atrelado a um numero baixo no server.listen()
