c509100eeffce50b25c5fb5994d6427812864cd1
OmarMesqq <omarmsqt@gmail.com>
2023-09-22 23:59:34 -0300

    test: :art: improve test

--- server/server_test.go
+++ server/server_test.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"testing"
 )
 
-// Toma um ponteiro para o struct T do testing
 func TestBindPort(reporter *testing.T) {
-	testListener, err := net.Listen("tcp", "127.0.0.1:9001")
+	busyPort := 9001
+	expectedPort := 9002
+	testListener, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", busyPort))
 	if err != nil {
 		reporter.Fatalf("Failed to create a testListener: %v", err)
 	}
 	defer testListener.Close()
 
-	port := 9001
-	serverSocket, boundPort := bindPort(port)
+	serverSocket, boundPort := bindPort(busyPort)
 	// defer serverSocket.Close()
 
 	if serverSocket == nil {
 		reporter.Error("Expected a non-nil server socket")
 	}
 
-	if boundPort != port {
-		reporter.Errorf("Expected bound port to be %d, but got %d", port, boundPort)
+	if boundPort != expectedPort {
+		reporter.Errorf("Expected bound port to be %d, but got %d", busyPort, boundPort)
 	}
 }
