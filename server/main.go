c4a92b14ea4ed900fa641e2f6c21fc65572916e0
OmarMesqq <omarmsqt@gmail.com>
2023-09-22 22:17:23 -0300

    refactor: :recycle: ordena comentarios e debito tecnico

--- server/main.go
+++ server/main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func bindPort(port int) (net.Listener, int) {
-	// Loop infinito para para criar um socket TCP conectado a uma porta
 	for {
 		serverSocket, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
 		if err == nil {
@@ -49,30 +48,9 @@ func handleClient(clientSocket net.Conn, count int) {
 
 	fmt.Printf("[*] Cliente novo (número %d) no endereço: %s\n", count, clientSocket.RemoteAddr())
 
-	// Começa a interação
-	clientSocket.Write(toBytesSlice("Toc Toc"))
+	clientSocket.Write(toBytesSlice("Toc Toc")) // começa a interação
 
-	// Espera "quem é?"
-	response, err := clientSocket.Read(clientBuffer)
-	if err != nil {
-		// Checa se erro é erro de rede. Se for, "ok" é verdade e atribui o erro a netErr.
-		// Depois, verifica se "ok" é verdade. Se for, verifica se o erro é de timeout.
-		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-			clientSocket.Write(toBytesSlice("\nVOCE EXPLODIU!\n"))
-			fmt.Printf("[*] Cliente %d desconectado por inatividade\n", count)
-			return
-		}
-		fmt.Printf("[*] Cliente %d encerrou a conexão\n", count)
-		return
-	}
-
-	fmt.Printf("[*] Cliente %d diz: %s\n", count, string(clientBuffer[:response]))
-
-	// Primeira parte da piada
-	clientSocket.Write(toBytesSlice(intro))
-
-	// Espera "fulano quem?"
-	response, err = clientSocket.Read(clientBuffer)
+	response, err := clientSocket.Read(clientBuffer) // espera "quem é?"
 	if err != nil {
 		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
 			clientSocket.Write(toBytesSlice("\nVOCE EXPLODIU!\n"))
@@ -82,10 +60,22 @@ func handleClient(clientSocket net.Conn, count int) {
 		fmt.Printf("[*] Cliente %d encerrou a conexão\n", count)
 		return
 	}
-
 	fmt.Printf("[*] Cliente %d diz: %s\n", count, string(clientBuffer[:response]))
 
-	// Punchline
+	clientSocket.Write(toBytesSlice(intro)) // primeira parte da piada
+
+	response, err = clientSocket.Read(clientBuffer) // espera "fulano quem?"
+	if err != nil {
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			clientSocket.Write(toBytesSlice("\nVOCE EXPLODIU!\n"))
+			fmt.Printf("[*] Cliente %d desconectado por inatividade\n", count)
+			return
+		}
+		fmt.Printf("[*] Cliente %d encerrou a conexão\n", count)
+		return
+	}
+	fmt.Printf("[*] Cliente %d diz: %s\n", count, string(clientBuffer[:response]))
+
 	clientSocket.Write(toBytesSlice(punchline))
 
 	fmt.Printf("[*] Conexão com cliente %d terminou com sucesso\n", count)
@@ -94,9 +84,3 @@ func handleClient(clientSocket net.Conn, count int) {
 func toBytesSlice(str string) []byte {
 	return []byte(str)
 }
-
-/*
-Obs.: A linguagem Go lida com erros de uma forma diferente da utilizada em outras linguagens.
-Em Go, erros são valores de retorno de funções. Se uma função retorna um erro,
-é responsabilidade de quem a chamou verificar se o erro é nil ou não.
-*/
