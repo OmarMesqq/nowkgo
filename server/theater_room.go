7790d06938071ec438f3cfaddb58f6cc00cb19aa
OmarMesqq <omarmsqt@gmail.com>
2023-12-12 22:52:46 -0300

    refactor: renomeia struct do teatro

--- server/theater_room.go
+++ server/theater_room.go
@@ -1,14 +1,14 @@
 package main
 
-type TheaterRoom struct {
+type Theater struct {
 	peopleInRoom       int
 	maxTheaterCapacity int
 }
 
-func (room *TheaterRoom) IsThereRoom() bool {
+func (room *Theater) IsThereRoom() bool {
 	return room.peopleInRoom < room.maxTheaterCapacity
 }
 
-func (room *TheaterRoom) IsEmpty() bool {
+func (room *Theater) IsEmpty() bool {
 	return room.peopleInRoom == 0
 }
